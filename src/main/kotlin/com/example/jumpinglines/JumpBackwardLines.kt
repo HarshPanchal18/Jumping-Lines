6df1d4c78fa39dc62243401479f4bf906feb8f73
Harsh Panchal <harshhhh1803@gmail.com>
2023-10-08 12:34:53 +0530

    Created Classes for displaying plugin settings in ide settings dialog
    Implemented Backward jumping

--- src/main/kotlin/com/example/jumpinglines/JumpBackwardLines.kt
+++ src/main/kotlin/com/example/jumpinglines/JumpBackwardLines.kt
@@ -6,34 +6,34 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.editor.Editor
 
-class JumpLines : AnAction() {
+class JumpBackwardLines : AnAction() {
+
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project
         val editor: Editor = e.getRequiredData(CommonDataKeys.EDITOR)
+        val document = editor.document
 
         if (project == null)
             return
 
         val caretModel = editor.caretModel
         val currentOffset = caretModel.offset
-        val linesToJump = 3 // Lines to jump, Positive for down, Negative for up
+
+        val linesToJump = -2 // Lines to jump, Positive for down, Negative for up
 
         // Calculate the new caret position
-        val document = editor.document
-        val lineCount = document.lineCount
         val currentLineNumber = document.getLineNumber(currentOffset)
         val newLineNumber = currentLineNumber + linesToJump
 
         // Ensure the new line number is within valid bounds
-        val validLineNumber = newLineNumber.coerceIn(0, lineCount - 1)
+        val validLineNumber = newLineNumber.coerceIn(0, document.lineCount - 1)
         val newOffset = document.getLineStartOffset(validLineNumber)
-
         caretModel.moveToOffset(newOffset)
     }
 
     override fun update(e: AnActionEvent) {
         val project = e.project
-        e.presentation.isEnabled = project!=null
+        e.presentation.isEnabled = project != null
     }
 
     override fun getActionUpdateThread(): ActionUpdateThread {
---
+++ src/main/kotlin/com/example/jumpinglines/JumpLinesSettingPanel.kt
@@ -0,0 +1,28 @@
+package com.example.jumpinglines
+
+import java.awt.BorderLayout
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JTextField
+
+class JumpLinesSettingPanel {
+
+    val panel = JPanel()
+    private val numberOfLinesTextField = JTextField(5)
+
+    init {
+        panel.layout = BorderLayout()
+        val label = JLabel("Number of lines to jump:")
+        panel.add(label, BorderLayout.WEST)
+        panel.add(numberOfLinesTextField, BorderLayout.CENTER)
+    }
+
+    fun getNumberOfLines(): Int {
+        return numberOfLinesTextField.text.toIntOrNull() ?: 3
+    }
+
+    fun setNumberOfLines(lines: Int) {
+        numberOfLinesTextField.text = lines.toString()
+    }
+
+}
