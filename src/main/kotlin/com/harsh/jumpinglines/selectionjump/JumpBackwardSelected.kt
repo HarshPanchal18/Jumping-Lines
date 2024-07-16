cf6e3e9408349e4557a3a90e8819493712b5b655
Harsh Panchal <harshhhh1803@gmail.com>
2024-07-16 11:57:38 +0530

    Implementing jumps along with selection over the folded regions

--- src/main/kotlin/com/harsh/jumpinglines/selectionjump/JumpBackwardSelected.kt
+++ src/main/kotlin/com/harsh/jumpinglines/selectionjump/JumpBackwardSelected.kt
@@ -3,6 +3,7 @@ package com.harsh.jumpinglines.selectionjump
 import com.harsh.jumpinglines.notification.showNotification
 import com.harsh.jumpinglines.utils.currentBackwardNoOfLines
 import com.harsh.jumpinglines.utils.editor
+import com.harsh.jumpinglines.utils.getTargetOffsetBackward
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.editor.*
@@ -22,25 +23,25 @@ class JumpBackwardSelected : DumbAwareAction() {
 			val currentOffset: Int = caretModel.offset
 			val scrollingModel: ScrollingModel = editor.scrollingModel
 			val selectionModel: SelectionModel = editor.selectionModel
+			val foldingModel: FoldingModel = editor.foldingModel
 
 			val startOffset: Int =
 				if (selectionModel.hasSelection()) selectionModel.leadSelectionOffset else currentOffset
 
 			// Calculate the new caret position
-			val currentLineNumber: Int = document.getLineNumber(currentOffset)
-			val newLineNumber: Int = currentLineNumber - currentBackwardNoOfLines
+			val targetOffset = getTargetOffsetBackward(document, foldingModel, currentOffset, currentBackwardNoOfLines)
 
-			// Ensure the new line number is within valid bounds
-			val validLineNumber: Int = newLineNumber.coerceIn(0, maximumValue = document.lineCount - 1)
-			val newOffset: Int = document.getLineStartOffset(validLineNumber)
+			caretModel.moveToOffset(targetOffset)
 
-			caretModel.moveToOffset(newOffset)
+			// Capture the original column before moving
+			// val originalColumn = caretModel.logicalPosition.column
 
 			// extend the selection
-			selectionModel.setSelection(/* startOffset = */ startOffset, /* endOffset = */ caretModel.offset)
+			selectionModel.setSelection(/* startOffset = */ startOffset,/* endOffset = */ caretModel.visualLineStart)
 
 			// Scrolling editor along with the cursor
-			val newPosition = LogicalPosition(/* line = */ validLineNumber, /* column = */ 0)
+			val newLineNumber = document.getLineNumber(targetOffset)
+			val newPosition = LogicalPosition(/* line = */ newLineNumber, /* column = */ 0)
 			caretModel.moveToLogicalPosition(newPosition)
 
 			scrollingModel.scrollTo(newPosition, ScrollType.RELATIVE)
@@ -59,4 +60,5 @@ class JumpBackwardSelected : DumbAwareAction() {
 	override fun getActionUpdateThread(): ActionUpdateThread {
 		return ActionUpdateThread.EDT
 	}
+
 }
--- src/main/kotlin/com/harsh/jumpinglines/selectionjump/JumpForwardSelected.kt
+++ src/main/kotlin/com/harsh/jumpinglines/selectionjump/JumpForwardSelected.kt
@@ -3,6 +3,7 @@ package com.harsh.jumpinglines.selectionjump
 import com.harsh.jumpinglines.notification.showNotification
 import com.harsh.jumpinglines.utils.currentForwardNoOfLines
 import com.harsh.jumpinglines.utils.editor
+import com.harsh.jumpinglines.utils.getTargetOffsetForward
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.editor.*
@@ -22,25 +23,22 @@ class JumpForwardSelected : DumbAwareAction() {
 			val currentOffset: Int = caretModel.offset
 			val scrollingModel: ScrollingModel = editor.scrollingModel
 			val selectionModel: SelectionModel = editor.selectionModel
+			val foldingModel: FoldingModel = editor.foldingModel
 
 			// If there is a selection, start from the beginning of the selection; otherwise, start from the current caret position
 			val startOffset: Int =
 				if (selectionModel.hasSelection()) selectionModel.leadSelectionOffset else currentOffset
 
 			// Calculate the new caret position
-			val currentLineNumber: Int = document.getLineNumber(currentOffset)
-			val newLineNumber: Int = currentLineNumber + currentForwardNoOfLines
+			val targetOffset = getTargetOffsetForward(document, foldingModel, currentOffset, currentForwardNoOfLines)
 
-			// Ensure the new line number is within valid bounds
-			val validLineNumber: Int = newLineNumber.coerceIn(0, maximumValue = document.lineCount - 1)
-			val newOffset: Int = document.getLineStartOffset(validLineNumber)
-
-			caretModel.moveToOffset(newOffset)
+			caretModel.moveToOffset(targetOffset)
 
 			// extend the selection
 			selectionModel.setSelection(/* startOffset = */ startOffset, /* endOffset = */ caretModel.offset)
 
 			// Scrolling editor along with the cursor
+			val newLineNumber = document.getLineNumber(targetOffset)
 			val newPosition = LogicalPosition(/* line = */ newLineNumber, /* column = */ 0)
 			caretModel.moveToLogicalPosition(newPosition)
 
